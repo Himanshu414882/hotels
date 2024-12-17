de6556404389a002ef106536e126af6b1b788624
Himanshu Sharma <hs414882@gmail.com>
2024-12-17 11:58:09 +0530

    final hotels app

---
+++ auth.js
@@ -0,0 +1,27 @@
+// sets up Passport with a local authentication strategy, using a Person model for user data. - Auth.js file
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Person = require('./models/Person'); // Adjust the path as needed
+
+passport.use(new LocalStrategy(async (username, password, done) => {
+    //authentication logic here
+    try {
+        // console.log('Received credentials:', username, password);
+        const user = await Person.findOne({ username });
+        if (!user)
+            return done(null, false, { message: 'Incorrect username.' });
+        
+        
+        const isPasswordMatch = await user.comparePassword(password);
+       //const isPasswordMatch = user.password === password ? true: false;
+        if (isPasswordMatch)
+            return done(null, user);
+        else
+            return done(null, false, { message: 'Incorrect password.' })
+    } catch (error) {
+        return done(error);
+    }
+}));
+
+module.exports = passport; // Export configured passport
--- server.js
+++ server.js
@@ -59,11 +59,22 @@ const express = require('express')
 const app = express()               // app name have all functionalities of express
 const db = require('./db');
 require('dotenv').config();
+const passport = require('./auth');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
+
+// Middleware Function
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+    next(); // Move on to the next phase
+}
+app.use(logRequest);
+
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate('local', {session: false})
 
 
 app.get('/', function (req, res) {     //to get the data
